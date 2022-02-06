4330307fb474e3d75819666ec49d3a91f4555e82
jack-carling <jack@carling.se>
2022-02-06 17:24:49 +0100

    Added Service Worker

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { registerSW } from 'virtual:pwa-register';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+registerSW();
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ includeAssets: ['site.webmanifest', 'favicon.ico', 'favicon-32x32.png', 'favicon-16x16.png'] }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
