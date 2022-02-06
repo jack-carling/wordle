d77f34b8f47405ce4eff702f6c9f30efb902692f
jack-carling <jack@carling.se>
2022-02-06 17:32:00 +0100

    Heroku redirect to SSL

--- heroku/index.js
+++ heroku/index.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect').default;
 const app = express();
+
+app.use(sslRedirect());
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-let port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server started on port ' + port));
